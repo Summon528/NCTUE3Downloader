feac6f7e688f79d2095bc3bf2b24ac98779ab57c
Summon528 <cody880528@hotmail.com>
2017-09-24 21:13:07 +0800

    Rewrite program using API

--- e3downloader.py
+++ e3downloader.py
@@ -1,88 +1,221 @@
 import requests
-import re
-import urllib.parse
+import xml.etree.ElementTree
+import urllib
+import os
+import datetime
+import sys
 from multiprocessing import Queue
-from bs4 import BeautifulSoup
 
-account = input('學號')
-pwd = input('密碼')
-session = requests.Session()
-baseUrl = "http://e3.nctu.edu.tw/NCTU_Easy_E3P/lms3/"
-url = "https://dcpc.nctu.edu.tw/index.aspx"
-print(url)
-r = session.get(url)
-print(r.status_code)
-soup = BeautifulSoup(r.content, "html.parser")
-__VIEWSTATE = soup.find('input', {"name": "__VIEWSTATE"}).get('value')
-__VIEWSTATEGENERATOR = soup.find(
-    'input', {"name": "__VIEWSTATEGENERATOR"}).get('value')
-__EVENTVALIDATION = soup.find(
-    'input', {"name": "__EVENTVALIDATION"}).get('value')
+BASE_URL = "http://e3.nctu.edu.tw/mService/service.asmx/"
+HEADERS = {
+    'content-type': 'application/x-www-form-urlencoded',
+}
 
-data = {"txtAccount": account, "txtPwd": pwd,
-        "__VIEWSTATE": __VIEWSTATE,
-        "__VIEWSTATEGENERATOR": __VIEWSTATEGENERATOR,
-        "__EVENTVALIDATION": __EVENTVALIDATION,
-        "__VIEWSTATEENCRYPTED": "",
-        "btnLoginIn": "SIGH IN"}
+try:
+    ACCOUNT = sys.argv[1]
+    PASSWORD = sys.argv[2]
+except IndexError:
+    ACCOUNT = input('學號 ')
+    PASSWORD = input('密碼 ')
 
-print(url)
-r = session.post(url, data=data)
-print(r.status_code)
+while True:
+    try:
+        data = {"account": ACCOUNT, "password": PASSWORD}
+        r = requests.post(BASE_URL + 'Login', data=data, headers=HEADERS)
+        root = xml.etree.ElementTree.fromstring(r.content)
+        LoginTicket = root[0].text
+        AccountId = root[1].text
+        print("Login Success")
+        break
+    except:
+        print("Login Fail")
+        ACCOUNT = input('學號 ')
+        PASSWORD = input('密碼 ')
+        continue
 
-soup = BeautifulSoup(r.content, "html.parser")
-__VIEWSTATE = soup.find('input', {"name": "__VIEWSTATE"}).get('value')
-__VIEWSTATEGENERATOR = soup.find(
-    'input', {"name": "__VIEWSTATEGENERATOR"}).get('value')
-content = r.content.decode('utf-8')
-content = content[content.find("<!--當期課程 Start-->")
-                               :content.find("<!--當期課程 End-->")]
-soup = BeautifulSoup(content, "html.parser")
-for i in soup.findAll('a', {'class': 'g2'}):
-    regex0 = r"javascript:__doPostBack\('([^']*)"
-    data = {"__VIEWSTATE": __VIEWSTATE,
-            "__VIEWSTATEGENERATOR": __VIEWSTATEGENERATOR,
-            "__EVENTTARGET": re.findall(regex0, i['href'])}
-    url = baseUrl + "enter_course_index.aspx"
-    print(url)
-    r = session.post(url, data=data)
-    print(r.status_code)
+try:
+    f = open('lastTime.txt', 'r')
+    try:
+        lastTime = datetime.datetime.strptime(f.read(), '%Y/%m/%d %H:%M:%S')
+        print("last run:" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    except:
+        print("lastTime.txt is corrupted.")
+        lastTime = datetime.datetime(1970, 1, 1, 0, 0, 0)
+    f.close()
+except FileNotFoundError:
+    print("Download all files.")
+    lastTime = datetime.datetime(1970, 1, 1, 0, 0, 0)
 
-    url = baseUrl + "stu_materials_document_list.aspx"
-    print (url)
-    r = session.get(url)
-    print(r.status_code)
-    regex = r"<a[^']*'([^,]*)[^>]*>\[&nbsp;view&nbsp;\]"
-    content = r.content.decode('utf-8')
-    for j in re.findall(regex, content):
-        url = "http://e3.nctu.edu.tw/NCTU_EASY_E3P/LMS3/" + j
-        print(url)
-        r2 = session.get(url)
-        print(r2.status_code)
-        content2 = r2.content.decode('utf-8')
-        regex2 = r"openDialog_hWin_[a-z0-9]*\('([^,]*)"
-        try:
-            url = "http://e3.nctu.edu.tw/NCTU_EASY_E3P/LMS3/" +  re.findall(regex2, content2)[1]
-        except IndexError:
-            continue
-        print(url)
-        r3 = session.get(url)
-        print('r3')
-        print(r3.status_code)
-        soup = BeautifulSoup(r3.content, "html.parser")
-        url = "http://e3.nctu.edu.tw/NCTU_EASY_E3P/LMS3/" + \
-            soup.find('a')['href']
-        print(url)
-        r4 = session.get(url, stream=True)
-        print(r4.status_code)
-        try:
-            fileName = r4.headers['Content-Disposition']
-        except KeyError:
-            continue
-        fileName = fileName[fileName.find('=') + 1:]
-        fileName = urllib.parse.unquote(fileName)
-        print (fileName)
-        with open(fileName, 'wb') as f:
-            for chunk in r4.iter_content(chunk_size=1024):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
+programStartTime = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+data = {"loginTicket": LoginTicket, "accountId": AccountId, 'role': 'stu'}
+r = requests.post(BASE_URL + 'GetCourseList', data=data, headers=HEADERS)
+root = xml.etree.ElementTree.fromstring(r.content)
+CourseId = []
+CourseName = []
+for i in root:
+    CourseId.append(i[0].text)
+    CourseName.append(i[3].text)
+
+
+for i in range(len(CourseId)):
+    print(CourseName[i])
+    print('課程講義')
+    try:
+        os.mkdir(CourseName[i])
+    except FileExistsError:
+        pass
+    try:
+        os.mkdir(os.path.join(CourseName[i], '課程講義'))
+    except FileExistsError:
+        pass
+    data = {"loginTicket": LoginTicket,
+            "courseId": CourseId[i], 'docType': '1'}
+    r = requests.post(BASE_URL + 'GetMaterialDocList',
+                      data=data, headers=HEADERS)
+    root = xml.etree.ElementTree.fromstring(r.content)
+    DisplayName = []
+    DocumentId = []
+    for j in root:
+        DisplayName.append(j[2].text)
+        DocumentId.append(j[11].text)
+    for j in DocumentId:
+        data = {"loginTicket": LoginTicket, "resId": j,
+                'metaType': '10', 'courseId': CourseId[i]}
+        r = requests.post(BASE_URL + 'GetAttachFileList',
+                          data=data, headers=HEADERS)
+        root = xml.etree.ElementTree.fromstring(r.content)
+
+        for k in root:
+            fileTime = datetime.datetime.strptime(
+                k[8].text, '%Y/%m/%d %H:%M:%S')
+            print(k[3].text)
+            if fileTime >= lastTime:
+                r2 = requests.get(k[4].text, stream=True)
+                try:
+                    fileName = r2.headers['Content-Disposition']
+                except KeyError:
+                    continue
+                fileName = fileName.encode('latin-1').decode('utf-8', 'ignore')
+                fileName = fileName[fileName.find(
+                    '=') + 2:fileName.find(',') - 1]
+                fileName = urllib.parse.unquote(fileName)
+                segment = int(r2.headers['content-length']) // 80
+                cnt = 0
+                with open(os.path.join(CourseName[i], '課程講義', fileName), 'wb') as f:
+                    sum = 0
+                    for chunk in r2.iter_content(chunk_size=1024):
+                        if sum > segment * cnt:
+                            cnt += 1
+                            print('#', end='', flush=True)
+                            if segment < 1024:
+                                for k in range(((sum - (segment * cnt)) // segment)):
+                                    cnt += 1
+                                    print('#', end='', flush=True)
+                        sum += 1024
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                print('')
+            else:
+                print("skip")
+
+    print('參考資料')
+    try:
+        os.mkdir(os.path.join(CourseName[i], '參考資料'))
+    except FileExistsError:
+        pass
+    data = {"loginTicket": LoginTicket,
+            "courseId": CourseId[i], 'docType': '1'}
+    r = requests.post(BASE_URL + 'GetMaterialDocList',
+                      data=data, headers=HEADERS)
+    root = xml.etree.ElementTree.fromstring(r.content)
+    DisplayName = []
+    DocumentId = []
+    for j in root:
+        DisplayName.append(j[2].text)
+        DocumentId.append(j[11].text)
+    for j in DocumentId:
+        data = {"loginTicket": LoginTicket, "resId": j,
+                'metaType': '10', 'courseId': CourseId[i]}
+        r = requests.post(BASE_URL + 'GetAttachFileList',
+                          data=data, headers=HEADERS)
+        root = xml.etree.ElementTree.fromstring(r.content)
+        for k in root:
+            fileTime = datetime.datetime.strptime(
+                k[8].text, '%Y/%m/%d %H:%M:%S')
+            print(k[3].text)
+            if fileTime >= lastTime:
+                r2 = requests.get(k[4].text, stream=True)
+                try:
+                    fileName = r2.headers['Content-Disposition']
+                except KeyError:
+                    continue
+                fileName = fileName.encode('latin-1').decode('utf-8')
+                fileName = fileName[fileName.find(
+                    '=') + 2:fileName.find(',') - 1]
+                fileName = urllib.parse.unquote(fileName)
+                segment = int(r2.headers['content-length']) // 80
+                cnt = 0
+                with open(os.path.join(CourseName[i], '參考資料', fileName), 'wb') as f:
+                    sum = 0
+                    for chunk in r2.iter_content(chunk_size=1024):
+                        if sum > segment * cnt:
+                            cnt += 1
+                            print('#', end='', flush=True)
+                            if segment < 1024:
+                                for k in range(((sum - (segment * cnt)) // segment)):
+                                    cnt += 1
+                                    print('#', end='', flush=True)
+                        sum += 1024
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                    print('')
+
+            else:
+                print('skip')
+
+    print('作業')
+    try:
+        os.mkdir(os.path.join(CourseName[i], '作業'))
+    except FileExistsError:
+        pass
+    data = {"loginTicket": LoginTicket,
+            "courseId": CourseId[i], "accountId": AccountId, 'listType': 1}
+    r = requests.post(BASE_URL + 'GetStuHomeworkListWithAttach',
+                      data=data, headers=HEADERS)
+    root = xml.etree.ElementTree.fromstring(r.content)
+    if len(root) > 0:
+        for j in root[0][25]:
+            r2 = requests.get(j.text[:-1], stream=True)
+            try:
+                fileName = r2.headers['Content-Disposition']
+            except KeyError:
+                continue
+            fileName = fileName.encode('latin-1').decode('utf-8')
+            fileName = fileName[fileName.find(
+                '=') + 2:fileName.find(',') - 1]
+            fileName = urllib.parse.unquote(fileName)
+            print(fileName)
+            segment = int(r2.headers['content-length']) // 80
+            cnt = 0
+            if os.path.isfile(os.path.join(CourseName[i], '作業', fileName)) == False:
+                with open(os.path.join(CourseName[i], '作業', fileName), 'wb') as f:
+                    sum = 0
+                    for chunk in r2.iter_content(chunk_size=1024):
+                        if sum > segment * cnt:
+                            cnt += 1
+                            print('#', end='', flush=True)
+                        if segment < 1024:
+                            for k in range(((sum - (segment * cnt)) // segment)):
+                                cnt += 1
+                                print('#', end='', flush=True)
+                        sum += 1024
+                        if chunk:  # filter out keep-alive new chunks
+                            f.write(chunk)
+                    print('')
+            else:
+                print('skip')
+    print('')
+f = open('lastTime.txt', 'w')
+f.write(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+f.close()
